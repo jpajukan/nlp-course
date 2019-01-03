737577109aafec8ccf433120f8c3c633469a8773
sakkoja <joellehtela@gmail.com>
2019-01-03 14:35:01 +0200

    Added also human readable results file

--- wikify/wikify_test.py
+++ wikify/wikify_test.py
@@ -7,10 +7,10 @@ def main():
         stopwords = set(line.strip() for line in open('stopwords.txt'))
     except:
         stopwords = {}
-    replacetable = dict.fromkeys(map(ord, ","), None) # characters to remove
+    replacetable = dict.fromkeys(map(ord, ",\n"), None) # characters to remove
     with open("results.csv", "a") as r:
         r.write("corpus" + "," + "word to dissambiguate" + "," + "result" + "\n")
-    with open("../testing/testfile_all_tests.csv", "r") as f:
+    with open("../testing/combined/testfile_all_blocks_combined_and_cleaned.csv", "r") as f:
         rubbish = f.readline() #read the first line of the data to discard header
         for line in f.readlines():
             corpus,word = line.strip("\n").split(",")
@@ -20,6 +20,8 @@ def main():
                 result = result.translate(replacetable) #removes the commas from the result
             with open("results.csv", "a") as r:
                 r.write(corpus + "," + word + "," + result + "\n")
+            with open("results_readable.txt", "a") as r:
+                r.write("Corpus: " + corpus + "\n\n" + "Word: " + word + "\n\n" + "The result: " + result + "\n******************************************\n")
 
 if __name__ == "__main__":
     main()
